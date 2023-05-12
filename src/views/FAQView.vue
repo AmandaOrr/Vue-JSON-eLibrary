00b62eb400167b538438cf69047ce7f7f3ff9c53
AmandaOrr <aorr77@gmail.com>
2023-05-12 15:29:16 -0700

    pushing project changes

--- src/views/FAQView.vue
+++ src/views/FAQView.vue
@@ -14,6 +14,9 @@
             <li>Sunday: 8am - 8pm</li>
         </ul>
 
+        <h3>Frequently Asked Questions</h3>
+        <p>-Where can I find the list of available titles? --- by visiting the <span>Library</span> Tab from our menu</p>
+        <p>-Do you carry more than just books? ---- Yes! We carry books, dvds, and streaming media.</p>
     </div>
 </template>
 
@@ -24,7 +27,7 @@
     text-align: center;
     background-color: rgb(173, 171, 171); opacity: .9;
     width: 50%;
-  
+    padding-bottom: 4rem;
     margin: 0 auto;
     border-radius: 7px;
 }
@@ -41,12 +44,20 @@ h2 {
     margin-top: 1rem;
 }
 
+h3 {
+    font-family: 'Wix Madefor Display', sans-serif;
+}
+
 p{
     font-family: 'Wix Madefor Display', sans-serif;
-    font-size: 3rem;
+    font-size: 2rem;
     font-weight: 800;
     padding: 1rem;
     text-align: left;
+}
+
+span {
+    text-decoration: underline;
 }
 
 ul {
